91e8ea72bfb66107af099e8e30ee4c4fe6ee41df
Munawar Johar <106137102+MunawarJohar@users.noreply.github.com>
2024-01-27 23:28:10 +0500

    push source code

---
+++ dist/script.dev.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var circle = document.getElementById("circle");
+var upBtn = document.getElementById("upBtn");
+var downBtn = document.getElementById("downBtn");
+var rotateValue = circle.style.transform;
+var rotateSum;
+
+upBtn.onclick = function () {
+  rotateSum = rotateValue + "rotate(-90deg)";
+  circle.style.transform = rotateSum;
+};
+
+downBtn.onclick = function () {
+  rotateSum = rotateValue + "rotate(90deg)";
+  circle.style.transform = rotateSum;
+};
+//# sourceMappingURL=script.dev.js.map
